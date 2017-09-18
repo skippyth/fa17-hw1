d07e05019897ad9e753a32e95c9b6bd4bec0b334
Katherine Bigelow <kbigelow@berkeley.edu>
2017-09-18 12:31:14 -0700

    completed homework 1

--- foobar.rb
+++ foobar.rb
@@ -1,8 +1,17 @@
 class Foobar
 
   def self.baz(a)
-    # Class method
-    # Call with `Foobar.baz`
+    i = [];
+    a.each do |n|
+       x = n.to_i;
+       if x.even?
+         i += [x+2];
+       end
+    end
+    r = i.uniq.delete_if{|n| n>10}
+    sum = 0;
+    r.each{|n| sum+= n}
+    sum
   end
 end
 
